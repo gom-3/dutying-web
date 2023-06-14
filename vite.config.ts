c83ae63d6ce46fcefc7b6292c29be3dd4f0f902e
sjsjsj1246 <sjsjsj1246@gmail.com>
2023-06-14 17:25:13 +0900

    Fix: ts 에러로 인한 빌드 실패 해결

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    checker({
+      typescript: true,
+    }),
+  ],
   server: {
     host: 'localhost',
     port: 3000,
