52238af35ddd8270cc567958b633889834d37628
sjsjsj1246 <sjsjsj1246@gmail.com>
2023-06-17 13:52:54 +0900

    Fix: Vercel 권한 변경 후 커밋

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import checker from 'vite-plugin-checker';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
