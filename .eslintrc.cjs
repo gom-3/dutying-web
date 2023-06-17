ac15f5e23e8cda649173ba2f3362191b4fa5e415
sjsjsj1246 <sjsjsj1246@gmail.com>
2023-06-17 22:21:53 +0900

    Fix: Eslint Prettier config 수정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,10 +4,15 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    'prettier:recommended',
+    'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module', project: './tsconfig.json' },
+  parserOptions: {
+    createDefaultProgram: true,
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
