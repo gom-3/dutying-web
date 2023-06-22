fc117eaa0aec974913c47abbffbb054a373ede41
sjsjsj1246 <sjsjsj1246@gmail.com>
2023-06-23 03:30:59 +0900

    Asset 추가 ArrowDownIcon

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,3 +1,4 @@
+export { default as ArrowDownIcon } from './ArrowDownIcon';
 export { default as DutyIcon } from './DutyIcon';
 export { default as FoldIcon } from './FoldIcon';
 export { default as FullLogo } from './FullLogo';
