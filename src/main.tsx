b43a8e76f84e25cb5d6f4b86178e9feae641d457
sjsjsj1246 <sjsjsj1246@gmail.com>
2023-07-06 02:37:34 +0900

    Feat: Sentry 부착

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,27 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from 'App';
 import { worker } from '@mocks/browser';
+import * as Sentry from '@sentry/react';
 
 if (import.meta.env.DEV) {
   await worker.start();
 }
+
+Sentry.init({
+  dsn: 'https://5035f79c451043f4b6438a90817ff608@o4505477969084416.ingest.sentry.io/4505477970526208',
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: [/^https:\/\/dutying-web\.vercel\.app/],
+    }),
+    new Sentry.Replay(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 const container = document.getElementById('root') as HTMLElement;
 const element = (
