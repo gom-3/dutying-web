5666fa7af692efe1867acc6dd8d0778848dc5e89
sjsjsj1246 <sjsjsj1246@gmail.com>
2023-07-06 02:59:15 +0900

    Feat: GA 부착 (도메인 구매 이후 태그 수정 필요)

--- src/main.tsx
+++ src/main.tsx
@@ -2,28 +2,9 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from 'App';
-import { worker } from '@mocks/browser';
-import * as Sentry from '@sentry/react';
+import initializeApp from 'initializeApp';
 
-if (import.meta.env.DEV) {
-  await worker.start();
-}
-
-Sentry.init({
-  dsn: 'https://5035f79c451043f4b6438a90817ff608@o4505477969084416.ingest.sentry.io/4505477970526208',
-  integrations: [
-    new Sentry.BrowserTracing({
-      // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
-      tracePropagationTargets: [/^https:\/\/dutying-web\.vercel\.app/],
-    }),
-    new Sentry.Replay(),
-  ],
-  // Performance Monitoring
-  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
+initializeApp();
 
 const container = document.getElementById('root') as HTMLElement;
 const element = (
