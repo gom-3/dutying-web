8d1e794408e647290395edd9e9464962c4a93ffb
sjsjsj1246 <sjsjsj1246@gmail.com>
2023-07-13 13:06:11 +0900

    Feat: 듀티표 엑셀 변환 기능 개발

--- src/mocks/duty/data.ts
+++ src/mocks/duty/data.ts
@@ -13,13 +13,13 @@ export const shiftList: ShiftList = [
     shiftTypeId: 0,
     wardId: 1,
     name: '오프',
-    shortName: 'O',
+    shortName: '/',
     startTime: '00:00',
     endTime: '00:00',
     hotkey: ['/', 'o', 'O', '0'],
     isDefault: true,
     isOff: true,
-    color: '#5534E0',
+    color: '#465B7A',
   },
   {
     shiftTypeId: 1,
@@ -31,7 +31,7 @@ export const shiftList: ShiftList = [
     hotkey: ['D', 'd', 'ㅇ', '1'],
     isDefault: true,
     isOff: false,
-    color: '#D7EB2A',
+    color: '#4DC2AD',
   },
   {
     shiftTypeId: 2,
@@ -43,7 +43,7 @@ export const shiftList: ShiftList = [
     hotkey: ['E', 'e', 'ㄷ', '2'],
     isDefault: true,
     isOff: false,
-    color: '#EB39E8',
+    color: '#FF8BA5',
   },
   {
     shiftTypeId: 3,
@@ -55,7 +55,7 @@ export const shiftList: ShiftList = [
     hotkey: ['N', 'n', 'ㅜ', '3'],
     isDefault: true,
     isOff: false,
-    color: '#271F3E',
+    color: '#629CFF',
   },
 ];
 
