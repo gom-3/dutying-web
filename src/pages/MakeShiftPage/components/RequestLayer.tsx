73110c739abed3d7727f7f2ec43cb4a7e1a2dd00
sjsjsj1246 <sjsjsj1246@gmail.com>
2023-07-18 16:19:30 +0900

    Fix: 툴팁 위치 수정

--- src/pages/MakeShiftPage/components/RequestLayer.tsx
+++ src/pages/MakeShiftPage/components/RequestLayer.tsx
@@ -8,14 +8,16 @@ interface Props {
 function RequestLayer({ isAccept, request }: Props) {
   return (
     <div
-      className={`absolute left-[1px] z-10 flex h-[2.125rem] w-[2.125rem] justify-center rounded-[.5625rem] border-[.125rem]
+      className={`absolute left-[.0625rem] z-10 flex h-[2.125rem] w-[2.125rem] justify-center rounded-[.5625rem] border-[.125rem]
         ${isAccept ? 'border-[#06E738] bg-[#06e73833]' : 'border-[#0027F4] bg-[#0027f433]'}
       `}
     >
       {!isAccept && (
-        <div className="invisible relative top-[-21px] z-30 flex justify-center group-hover:visible">
+        <div className=" relative top-[-1.3125rem] z-30 flex justify-center group-hover:visible">
           <ChatIcon className="absolute h-[1.25rem] w-[1.0625rem]" fill={request.color} />
-          <p className="absolute font-poppins text-[.6rem] text-white">{request.shortName}</p>
+          <p className="absolute top-[-0.24rem] font-poppins text-[.6rem] text-white">
+            {request.shortName}
+          </p>
         </div>
       )}
     </div>
