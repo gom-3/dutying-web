8fdab5c42dce89663051217347d929b41d88cfff
sjsjsj1246 <sjsjsj1246@gmail.com>
2023-07-21 03:41:51 +0900

    라우트 정리

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,29 +1,23 @@
-import { Route, Routes } from 'react-router-dom';
-import { SHIFT, HOME, LOGIN, MEMBER, REDIRECT, ONBOARDING, ROOT } from '@libs/constant/path';
-import MakeShiftPage from './MakeShiftPage';
-import LoginPage from './LoginPage';
-import RegistMemberPage from './MemberPage/RegistMemberPage';
-import HomePage from './HomePage';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { SHIFT, HOME, MEMBER, ROOT } from '@libs/constant/path';
 import MainLayout from '@components/Layouts/MainLayout';
-import RedirectPage from './LoginPage/RedirectPage';
+import HomePage from './HomePage';
+import RegistMemberPage from './MemberPage/RegistMemberPage';
 import RequestPage from './RequestShiftPage';
-import SetAccount from './OnboardingPage/SetAccount';
-import SetWard from './OnboardingPage/SetWard';
-import LandingPage from './LandingPage';
-import NotAuthzLayout from '@components/Layouts/NotAuthzLayout';
 import SetShiftPage from './SetShiftPage';
+import MakeShiftPage from './MakeShiftPage';
 
 export const Router = () => {
   return (
     <Routes>
-      <Route path={ROOT} element={<LandingPage />} />
-      <Route path={REDIRECT} element={<RedirectPage />} />
+      <Route path={ROOT} element={<Navigate to={SHIFT.MAKE} />} />
+      {/* <Route path={REDIRECT} element={<RedirectPage />} /> */}
       {/* 인증된 사용자가 접근할 수 없는 페이지 */}
-      <Route element={<NotAuthzLayout />}>
-        <Route path={LOGIN} element={<LoginPage />} />
-        <Route path={ONBOARDING.ACCOUNT} element={<SetAccount />} />
-        <Route path={ONBOARDING.WARD} element={<SetWard />} />
-      </Route>
+      {/* <Route element={<NotAuthzLayout />}> */}
+      {/* <Route path={LOGIN} element={<LoginPage />} /> */}
+      {/* <Route path={ONBOARDING.ACCOUNT} element={<SetAccount />} /> */}
+      {/* <Route path={ONBOARDING.WARD} element={<SetWard />} /> */}
+      {/* </Route> */}
       {/* 인증되지 않은 사용자가 접근할 수 없는 페이지 */}
       <Route element={<MainLayout />}>
         <Route path={HOME} element={<HomePage />} />
