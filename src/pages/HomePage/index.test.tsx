15875be78b7de9b363eef2b97e3f480c25e5143f
sjsjsj1246 <sjsjsj1246@gmail.com>
2023-07-21 16:46:39 +0900

    Fix: Page Rendering test 제거

--- src/pages/HomePage/index.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@libs/util/test-utils';
-import HomePage from '.';
-
-describe('HomePage 렌더링 테스트', () => {
-  it('충돌 없이 렌더링 된다', () => {
-    render(<HomePage />);
-  });
-});
