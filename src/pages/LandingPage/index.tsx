0db20949dc1be31c4a817d3888a2f91f76c0348a
sjsjsj1246 <sjsjsj1246@gmail.com>
2023-10-23 21:53:08 +0900

    Fix: dvh -> svh

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -114,7 +114,7 @@ function LandingPage() {
       </div>
 
       {/*메인*/}
-      <div className='xl:h-[${innerHeight}] h-[100dvh] w-screen bg-[url("/img/landing_mobile_1.png")] bg-cover bg-center bg-no-repeat pt-[60px]  xl:relative xl:bg-[url("/img/landing_1.png")] xl:pt-0'>
+      <div className='xl:h-[${innerHeight}] h-[100svh] w-screen bg-[url("/img/landing_mobile_1.png")] bg-cover bg-center bg-no-repeat pt-[60px]  xl:relative xl:bg-[url("/img/landing_1.png")] xl:pt-0'>
         <div className="mx-auto flex h-full w-[85%] flex-col pt-[42px] xl:container xl:relative xl:top-[50%] xl:h-fit xl:translate-y-[-50%] xl:pt-0">
           <h1 className="font-line text-[32px] font-bold leading-[42px] text-main-1 xl:text-[4rem] xl:leading-[5.4375rem] xl:text-white">
             근무표,
@@ -206,7 +206,7 @@ function LandingPage() {
       <div
         id="web"
         ref={webSection}
-        className='xl:h-[${innerHeight}] h-[100dvh] w-screen bg-[url("/img/landing_mobile_2.png")] bg-cover bg-center bg-no-repeat  xl:bg-[url("/img/landing_2.png")]'
+        className='xl:h-[${innerHeight}] h-[100svh] w-screen bg-[url("/img/landing_mobile_2.png")] bg-cover bg-center bg-no-repeat  xl:bg-[url("/img/landing_2.png")]'
       >
         <div className="mx-auto flex h-full w-[85%] flex-col pt-[124px] xl:container xl:mx-auto xl:mt-[8.875rem] xl:items-end">
           <div className="flex items-center gap-[8px] xl:w-[28.125rem]">
@@ -231,7 +231,7 @@ function LandingPage() {
       </div>
 
       {/*메인 3*/}
-      <div className='xl:h-[${innerHeight}] h-[100dvh] w-screen bg-[url("/img/landing_mobile_3.png")] bg-cover bg-center bg-no-repeat  xl:bg-[url("/img/landing_3.png")]'>
+      <div className='xl:h-[${innerHeight}] h-[100svh] w-screen bg-[url("/img/landing_mobile_3.png")] bg-cover bg-center bg-no-repeat  xl:bg-[url("/img/landing_3.png")]'>
         <div className="mx-auto flex h-full w-[85%] flex-col pt-[64px] xl:container xl:mx-auto xl:mt-[8.875rem] xl:items-start">
           <div className="flex items-center gap-[8px] xl:w-[28.125rem]">
             <div className="flex h-[22px] items-center rounded-[5px] bg-white px-[6px] font-poppins text-[14px] text-main-1 xl:h-[1.875rem] xl:px-[.5rem] xl:text-[1.25rem]">
@@ -260,7 +260,7 @@ function LandingPage() {
       <div
         id="mobile"
         ref={mobileSection}
-        className='xl:h-[${innerHeight}] h-[100dvh] w-screen bg-[url("/img/landing_mobile_4.png")] bg-cover bg-center bg-no-repeat  xl:bg-[url("/img/landing_4.png")]'
+        className='xl:h-[${innerHeight}] h-[100svh] w-screen bg-[url("/img/landing_mobile_4.png")] bg-cover bg-center bg-no-repeat  xl:bg-[url("/img/landing_4.png")]'
       >
         <div className="mx-auto flex h-full w-[85%] flex-col pt-[124px] xl:container xl:mx-auto xl:mt-[8.875rem] xl:items-start">
           <div className="flex items-center gap-[8px] xl:w-[28.125rem]">
@@ -285,7 +285,7 @@ function LandingPage() {
       </div>
 
       {/*메인 5*/}
-      <div className='h-[100dvh] w-screen bg-[url("/img/landing_mobile_5.png")] bg-cover bg-center bg-no-repeat xl:h-[100dvh]  xl:bg-[url("/img/landing_5.png")]'>
+      <div className='h-[100svh] w-screen bg-[url("/img/landing_mobile_5.png")] bg-cover bg-center bg-no-repeat xl:h-[100svh]  xl:bg-[url("/img/landing_5.png")]'>
         <div className="mx-auto flex h-full w-[85%] flex-col pt-[124px] xl:container xl:mx-auto xl:mt-[8.875rem] xl:items-end">
           <div className="flex items-center gap-[8px] xl:w-[28.125rem]">
             <div className="flex h-[22px] items-center rounded-[5px] bg-main-4 px-[6px] font-poppins text-[14px] text-main-1 xl:h-[1.875rem] xl:px-[.5rem] xl:text-[1.25rem]">
