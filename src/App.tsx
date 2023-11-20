0ac4b170882fbd9219b7f90dfa93cf8ceefbc799
sjsjsj1246 <sjsjsj1246@gmail.com>
2023-11-21 00:07:37 +0900

    Feat: 참여 이벤트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 
 function App() {
   useAuth(true);
+
   const setScreenHeight = () => {
     const vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
