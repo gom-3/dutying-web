926e54eff51f8ff930bf6609eeb30f3167a1d285
Inseo Hwang <sjsjsj1246@gmail.com>
2023-11-21 00:13:19 +0900

    Feat: 참여 이벤트 추가 (#87)
    * Feat: 참여 이벤트 추가
    
    * Release 0.11.0: 이벤트 팝업 개발

---
+++ src/components/Event/index.tsx
@@ -0,0 +1,71 @@
+import { BackCircle, CancelIcon, NextCircle } from '@assets/svg';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import './index.css';
+import Button from '@components/Button';
+
+interface PopupModalProps {
+  open: boolean;
+  onClose: () => void;
+}
+
+const Event = ({ open, onClose }: PopupModalProps) => {
+  return (
+    open && (
+      <div
+        className="fixed left-0 top-0 z-[999] flex h-screen w-screen items-center justify-center bg-[#0000005e]"
+        onClick={onClose}
+      >
+        <div
+          className=" flex h-[50rem] w-[40rem] flex-col justify-between rounded-[1.25rem] bg-white p-10"
+          onClick={(e) => e.stopPropagation()}
+        >
+          <div className="flex w-full justify-between">
+            <h1 className="font-apple text-[1.7rem] font-semibold">듀팅 고객 이벤트 (~11.21)</h1>
+            <CancelIcon className="h-[2rem] w-[2rem] cursor-pointer" onClick={onClose} />
+          </div>
+          <div className="mt-[1rem] w-full flex-1">
+            <Carousel
+              dynamicHeight
+              width="100%"
+              autoPlay
+              infiniteLoop
+              stopOnHover
+              showArrows
+              interval={3000}
+              showIndicators={false}
+              showThumbs={false}
+              statusFormatter={(current, total) => `${current} / ${total}`}
+              renderArrowPrev={(click) => (
+                <BackCircle
+                  className="absolute left-[1rem] top-[50%] z-10 h-[3rem] w-[3rem] translate-y-[-50%] cursor-pointer"
+                  onClick={click}
+                />
+              )}
+              renderArrowNext={(click) => (
+                <NextCircle
+                  className="absolute right-[1rem] top-[50%] z-10 h-[3rem] w-[3rem] translate-y-[-50%] cursor-pointer"
+                  onClick={click}
+                />
+              )}
+            >
+              <div className='h-[35rem] w-[35rem] min-w-[.0625rem] bg-[url("/img/event1.webp")] bg-cover bg-center'></div>
+              <div className='h-[35rem] w-[35rem] min-w-[.0625rem] bg-[url("/img/event2.webp")] bg-cover bg-center'></div>
+              <div className='h-[35rem] w-[35rem] min-w-[.0625rem] bg-[url("/img/event3.webp")] bg-cover bg-center'></div>
+            </Carousel>
+          </div>
+          <a href="https://abr.ge/twsrzwm" target="_blank" className="w-full">
+            <Button
+              className="mt-auto w-full rounded-lg py-[.5rem] text-[1.8rem] transition-all hover:bg-main-1 hover:text-white"
+              type="outline"
+            >
+              이벤트 참여하기
+            </Button>
+          </a>
+        </div>
+      </div>
+    )
+  );
+};
+
+export default Event;
