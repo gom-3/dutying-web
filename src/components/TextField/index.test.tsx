9a5ca2423525822483acd075ee17ab0eb671b47b
cksrb63 <cksr1@naver.com>
2024-02-28 22:03:30 +0900

    Test: TextField

--- src/components/TextField/index.test.tsx
+++ src/components/TextField/index.test.tsx
@@ -1,8 +1,30 @@
-import { render } from '@libs/util/test-utils';
+import { render, screen, userEvent } from '@libs/util/test-utils';
 import TextField from '.';
+import { expect, vi } from 'vitest';
 
-describe('TextField는', () => {
-  it('충돌 없이 렌더링 된다', () => {
+describe('TextField 컴포넌트', () => {
+  it('정상적으로 렌더링되어야 함', () => {
     render(<TextField />);
+    expect(screen.getByRole('textbox')).toBeInTheDocument();
+  });
+
+  it('사용자 입력마다 onChange가 실행되어야 함', async () => {
+    const spy = vi.fn();
+    render(<TextField onChange={spy} />);
+    const input = screen.getByRole('textbox');
+    await userEvent.type(input, 'test');
+    expect(spy).toHaveBeenCalledTimes(4);
+  });
+
+  it('error prop이 주어지면 에러 메세지를 표시해야 함', () => {
+    render(<TextField error="error" />);
+    expect(screen.getByText('error')).toBeInTheDocument();
+  });
+
+  it('error prop이 주어지면 TextField의 외곽선을 빨간색으로 변경해야 함', () => {
+    render(<TextField error="error" />);
+    expect(screen.getByText('error').previousElementSibling).toHaveClass(
+      'outline-red focus:outline-red'
+    );
   });
 });
